8291ed64b134f068fb2c56a63e8d6f59cc9f581b
Nico Krause <nico@le-space.de>
2019-04-30 19:58:17 +0500

    don't save in db when default value is undefined and not configured in settings.json

--- settings.js
+++ settings.js
@@ -5,18 +5,20 @@ export const name = 'settings';
 export function getSettings(_name,defaultValue){
 
     const settingsJsonValue = _.get(Meteor.settings, _name, defaultValue);
-    console.log('found settings for '+_name+' in json: app.debug:',settingsJsonValue);
+    console.log('found settings for '+_name+' in json:',settingsJsonValue);
 
     _name = _name.replace(/\./g, "___");
     const selector = {[_name]: { $exists: true }};
     const settingsDbVAlue = Settings.findOne(selector);
-    console.log('db: app.debug:',settingsDbVAlue);
+    //console.log('db: app.debug:',settingsDbVAlue);
     if(settingsDbVAlue===undefined){
-        console.log('setting for '+_name +' not found in db, adding it for for the first time.')
-        Settings.insert({[_name]: settingsJsonValue});
+        if(defaultValue!==undefined && settingsJsonValue!==undefined){
+            console.log('setting for '+_name +' not found in db - adding it.')
+            Settings.insert({[_name]: settingsJsonValue});
+        }
     }else{
-        console.log(' found settings for '+_name +' in db:',_.get(settingsDbVAlue,_name));
-        return settingsDbVAlue;
+        console.log('found settings for '+_name +' in db:',_.get(settingsDbVAlue,_name));
+        return _.get(settingsDbVAlue,_name);
     }
     return settingsJsonValue;
 }
