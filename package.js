471e6d98e28eec940aed0f89ae9d14811b30c0a1
Nico Krause <nico@le-space.de>
2019-05-02 17:31:54 +0500

    replaced lodash package

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: 'doichain:settings',
-  version: '0.1.5',
+  version: '0.1.6',
   // Brief, one-line summary of the package.
   summary: 'Loads and provides settings',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/Doichain/meteor-doichain-settings.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 const use   = [
-      'ecmascript',
-      'wanglian:lodash@0.0.1'
+        'ecmascript',
+        'erasaur:meteor-lodash','4.0.0',
     ];
 
 Package.onUse(function(api) {
